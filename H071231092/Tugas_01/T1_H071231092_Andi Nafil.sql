cfaa589b383f7874395621b206cced10d5f9e9f7
Andi Nafil F.H <135929479+Nark818@users.noreply.github.com>
2024-09-18 07:31:55 +0800

    Tugas 01

---
+++ H071231092/Tugas_01/T1_H071231092_Andi Nafil.sql
@@ -0,0 +1,68 @@
+CREATE DATABASE library;
+
+USE library;
+
+CREATE TABLE authors (
+	id INT PRIMARY KEY,
+	NAME VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Books (
+	id INT PRIMARY KEY,
+	isbn VARCHAR(13),
+	title VARCHAR(100) NOT NULL,
+	author_id INT,
+	FOREIGN KEY(author_id) REFERENCES authors(id) 
+);
+
+ALTER TABLE authors
+MODIFY id INT AUTO_INCREMENT;
+
+ALTER TABLE Books
+MODIFY id INT AUTO_INCREMENT;
+
+ALTER TABLE authors
+ADD nationality VARCHAR(50);
+DESCRIBE authors;
+
+ALTER TABLE Books 
+MODIFY isbn VARCHAR(13) UNIQUE;
+DESCRIBE Books;
+
+SHOW TABLES;
+DESCRIBE authors;
+DESCRIBE Books;
+
+CREATE TABLE members (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	phone_number CHAR(10),
+	join_date DATE NOT NULL,
+	membership_type VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE borrowings (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	member_id INT NOT NULL,
+	book_id INT NOT NULL,
+	borrow_date DATE NOT NULL,
+	return_date DATE,
+	FOREIGN KEY(member_id) REFERENCES authors(id),
+	FOREIGN KEY(book_id) REFERENCES Books(id)    
+);
+
+ALTER TABLE Books
+MODIFY title VARCHAR(150) NOT NULL, 
+MODIFY author_id INT NOT NULL,
+MODIFY isbn CHAR(13) UNIQUE,
+ADD published_year YEAR NOT NULL,
+ADD genre VARCHAR(50) NOT NULL,
+ADD copies_available INT NOT NULL; 
+DESCRIBE Books;
+
+ALTER TABLE authors
+MODIFY id INT NOT NULL,
+MODIFY nationality VARCHAR(50) NOT NULL;
+DESCRIBE authors
